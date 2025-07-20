9e05d50156eb6cc881a9a888e5957b44328542b2
Dmitry Romashov <framebassman@gmail.com>
2025-07-20 20:05:59 +0200

    Add an customers, orders, meals (#3)

---
+++ api/src/db/migrations/0001_tidy_jigsaw.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "customers" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"address" text NOT NULL,
+	"name" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "meals" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"price" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "order_meals" (
+	"order_id" integer NOT NULL,
+	"meal_id" integer NOT NULL,
+	"quantity" integer DEFAULT 1 NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "orders" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"sub_total" integer DEFAULT 0 NOT NULL,
+	"customer_id" integer,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DROP TABLE "todos" CASCADE;--> statement-breakpoint
+ALTER TABLE "order_meals" ADD CONSTRAINT "order_meals_order_id_orders_id_fk" FOREIGN KEY ("order_id") REFERENCES "public"."orders"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "order_meals" ADD CONSTRAINT "order_meals_meal_id_meals_id_fk" FOREIGN KEY ("meal_id") REFERENCES "public"."meals"("id") ON DELETE no action ON UPDATE no action;
