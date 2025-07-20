dbae1567a2b9a76039420778a8c5db5127ec778f
Dmitry Romashov <framebassman@gmail.com>
2025-07-20 17:47:58 +0200

    Add migrations

--- api/hub.config.ts
+++ api/hub.config.ts
@@ -1,15 +1,19 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { defineCollection, defineHub } from 'honohub';
+import { defineCollection, defineHub } from 'kolenkainc-honohub';
 import * as schema from './src/db/schema';
+import * as dotenv from 'dotenv';
 
-export const getHub = (connectionString: string) =>
-  defineHub({
-    db: drizzle(neon(connectionString), { schema }),
-    collections: [
-      defineCollection({
-        slug: 'todos',
-        schema: schema.todos
-      })
-    ]
-  });
+dotenv.config();
+const neonSql = neon(String(process.env.DATABASE_URL));
+const db = drizzle(neonSql, { schema });
+
+export default defineHub({
+  db,
+  collections: [
+    defineCollection({
+      slug: 'todos',
+      schema: schema.todos
+    })
+  ]
+});
---
+++ api/src/db/migrate.ts
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+
+dotenv.config();
+console.log(`process.env.DATABASE_URL: ${process.env.DATABASE_URL}`);
+const sql = neon(process.env.DATABASE_URL ?? '');
+
+const db = drizzle(sql);
+
+const main = async () => {
+  try {
+    await migrate(db, {
+      migrationsFolder: 'src/db/migrations'
+    });
+
+    console.log('Migration successful');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+main();
