e56ba46b1195aa1701f7716fe3bc47cd73b56ca3
Dmitry Romashov <framebassman@gmail.com>
2025-07-20 17:35:46 +0200

    Remove goods

--- goods/src/worker/db/migrations/0000_acoustic_secret_warriors.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "todos" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"status" boolean DEFAULT false NOT NULL,
-	"message" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
